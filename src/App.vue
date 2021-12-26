aa5a9f59088a71e3eba85f03ee55148800e83041
inkfarer <inkfarer@inkfarer.com>
2021-12-26 17:09:48 +0200

    Support scss; Fix obsolete snapshot

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style lang="scss">
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
