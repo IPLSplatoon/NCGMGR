228b1ff0025563d9bbe420ba98a57d107c450550
inkfarer <inkfarer@inkfarer.com>
2021-12-28 18:15:50 +0200

    Fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    indent: ['warn', 4, { SwitchCase: 1 }],
-    'symbol-description': 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
+    root: true,
+    env: {
+        node: true
     },
-    {
-      files: ['*.test.ts'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 0,
-        'import/first': 'off'
-      }
-    }
-  ]
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/standard',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['warn', 4, { SwitchCase: 1 }],
+        'symbol-description': 'off'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        },
+        {
+            files: ['*.test.ts'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 0,
+                'import/first': 'off'
+            }
+        }
+    ]
 }
