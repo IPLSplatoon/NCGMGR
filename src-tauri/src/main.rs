52e5962de92e805273970717ac202f97c28e8846
inkfarer <inkfarer@inkfarer.com>
2021-12-29 20:31:24 +0200

    Prevent panics in rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,10 +6,11 @@ windows_subsystem = "windows"
 use std::cmp::Ordering;
 use git2::{Direction, Remote, Repository};
 use tauri::{Menu, MenuItem, Submenu};
-use std::fs;
+use std::{fmt, fs};
 use std::path::Path;
 use std::process::Child;
 use itertools::Itertools;
+use unwrap_or::unwrap_ok_or;
 
 mod npm;
 mod log;
@@ -17,23 +18,28 @@ mod log;
 const NODECG_GIT_PATH: &str = "https://github.com/nodecg/nodecg.git";
 const NODECG_TAG: &str = "v1.8.1";
 
+fn format_error<F, T: fmt::Display>(msg: &str, err: T) -> Result<F, String> {
+    Err(format!("{}: {}", msg, err.to_string()))
+}
+
 fn clone_nodecg(path: &str) -> Result<String, String> {
-    match Repository::clone(NODECG_GIT_PATH, path) {
-        Ok(repo) => {
-            let (object, reference) = repo.revparse_ext(NODECG_TAG).expect("Object not found");
+    let repository = match Repository::clone(NODECG_GIT_PATH, path) {
+        Ok(repo) => repo,
+        Err(e) => return format_error("Failed to clone NodeCG", e)
+    };
+    let (object, reference) = match repository.revparse_ext(NODECG_TAG) {
+        Ok(parsed) => parsed,
+        Err(e) => return format_error("Could not parse revision", e)
+    };
 
-            repo.checkout_tree(&object, None).expect("Could not checkout");
+    unwrap_ok_or!(repository.checkout_tree(&object, None), e, { return format_error(&format!("Could not check out NodeCG {}", NODECG_TAG), e) });
 
-            match reference {
-                Some(gref) => repo.set_head(gref.name().unwrap()),
-                None => repo.set_head_detached(object.id())
-            }
-                .expect("Failed to set HEAD");
-
-            Ok("OK".to_string())
-        }
-        Err(e) => Err(format!("Failed to clone NodeCG: {}", e.to_string()))
+    match reference {
+        Some(gref) => repository.set_head(gref.name().unwrap()),
+        None => repository.set_head_detached(object.id())
     }
+        .or_else(|e| return format_error("Failed to set HEAD", e))
+        .and_then(|_| return Ok("OK".to_string()))
 }
 
 fn log_npm_install(handle: &tauri::AppHandle, mut child: Child, log_key: &str) -> Result<String, String> {
@@ -52,7 +58,7 @@ fn log_npm_install(handle: &tauri::AppHandle, mut child: Child, log_key: &str) -
                 }
             }
         }
-        Err(_) => Err(format!("Failed to install npm dependencies"))
+        Err(e) => format_error("Failed to install npm dependencies", e)
     }
 }
 
@@ -83,14 +89,19 @@ fn install_bundle(handle: tauri::AppHandle, bundle_name: String, bundle_url: Str
     let dir_bundles = format!("{}/bundles", nodecg_path);
     if !Path::new(&dir_bundles).exists() {
         log::emit(&handle, log_key, "Creating missing bundles directory");
-        fs::create_dir(dir_bundles).expect("Failed to create bundles directory");
+        unwrap_ok_or!(fs::create_dir(dir_bundles), e, { return format_error("Failed to create bundles directory", e) });
     }
 
     log::emit(&handle, log_key, "Fetching version list...");
-    let mut remote = Remote::create_detached(&bundle_url).expect("Could not create remote");
-    let connection = remote.connect_auth(Direction::Fetch, None, None).expect("Could not connect to remote");
-    let versions = connection
-        .list().expect("Could not get version list")
+    let mut remote = match Remote::create_detached(&bundle_url) {
+        Ok(remote) => remote,
+        Err(e) => return format_error("Could not create remote", e)
+    };
+    let connection = match remote.connect_auth(Direction::Fetch, None, None) {
+        Ok(connection) => connection,
+        Err(e) => return format_error("Could not connect to remote", e)
+    };
+    let versions = unwrap_ok_or!(connection.list(), e, { return format_error("Could not get version list", e) })
         .iter()
         .filter(|item| { item.name().starts_with("refs/tags/") })
         .map(|item| item.name().split("refs/tags/").last().unwrap())
@@ -113,17 +124,17 @@ fn install_bundle(handle: tauri::AppHandle, bundle_name: String, bundle_url: Str
                 let latest_version = versions.first().unwrap();
                 log::emit(&handle, log_key, &format!("Checking out version {}...", latest_version));
 
-                let (object, reference) = repo.revparse_ext(latest_version).expect("Object not found");
+                let (object, reference) = unwrap_ok_or!(repo.revparse_ext(latest_version), e, { return format_error("Could not parse ref", e) });
 
-                repo.checkout_tree(&object, None).expect("Could not checkout");
+                unwrap_ok_or!(repo.checkout_tree(&object, None), e, { return format_error("Checkout failed", e) });
 
-                match reference {
+                unwrap_ok_or!(match reference {
                     Some(gref) => repo.set_head(gref.name().unwrap()),
                     None => repo.set_head_detached(object.id())
-                }.expect("Failed to set HEAD");
+                }, e, { return format_error("Failed to set HEAD", e) })
             }
         },
-        Err(e) => panic!("Failed to clone bundle '{}': {}", bundle_name, e.to_string())
+        Err(e) => return format_error(&format!("Failed to clone bundle '{}'", bundle_name), e)
     }
 
     npm::install_npm_dependencies(&bundle_path).and_then(|child| {
