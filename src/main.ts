6b01848f5263d958a2bf7a864ecd423383181fff
inkfarer <inkfarer@inkfarer.com>
2022-03-18 17:23:13 +0200

    Add opaque background to window when the system does not support transparency effects

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { type, version } from '@tauri-apps/api/os'
 
-const pinia = createPinia()
+(async () => {
+    const useTransparentBg = await supportsTransparentBackground()
+    if (!useTransparentBg) {
+        document.body.classList.add('use-opaque-bg')
+    }
 
-createApp(App)
-    .use(pinia)
-    .mount('#app')
+    createApp(App)
+        .use(createPinia())
+        .mount('#app')
+})()
+
+async function supportsTransparentBackground () {
+    const osType = await type()
+    if (osType === 'Windows_NT') {
+        const osVersion = await version()
+        const buildNumber = Number(osVersion.split('.').pop())
+
+        return buildNumber >= 22000
+    } else return osType === 'Darwin'
+}
