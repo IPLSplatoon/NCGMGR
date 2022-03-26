6e8d619ca75a65231ae91a17486010a9c7f67d24
inkfarer <inkfarer@inkfarer.com>
2022-03-26 17:15:06 +0200

    Fix tests

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -2,6 +2,7 @@ import App from '@/App.vue'
 import { shallowMount } from '@vue/test-utils'
 import { InstallStatus, useNodecgStore } from '@/store/nodecg'
 import { createTestingPinia, TestingPinia } from '@pinia/testing'
+import { useDependencyStore } from '@/store/dependencyStore'
 
 describe('App', () => {
     let pinia: TestingPinia
@@ -10,7 +11,19 @@ describe('App', () => {
         pinia = createTestingPinia()
     })
 
-    it('shows bundle manager if nodecg is installed', () => {
+    it('hides bundle and install managers if nodejs is not installed', () => {
+        useDependencyStore().nodejs.version = null
+        const wrapper = shallowMount(App, { global: { plugins: [pinia] } })
+
+        const dependencyChecker = wrapper.find('dependency-checker-stub')
+        expect(dependencyChecker.exists()).toEqual(true)
+        expect(dependencyChecker.isVisible()).toEqual(true)
+        expect(wrapper.find('bundle-manager-stub').exists()).toEqual(false)
+        expect(wrapper.find('install-manager-stub').exists()).toEqual(false)
+    })
+
+    it('shows bundle manager if nodecg and nodejs are installed', () => {
+        useDependencyStore().nodejs.version = '12.45.2'
         const nodecgStore = useNodecgStore()
         nodecgStore.status.installStatus = InstallStatus.INSTALLED
         const wrapper = shallowMount(App, {
@@ -25,6 +38,7 @@ describe('App', () => {
     })
 
     it('hides bundle manager if nodecg is not installed but can be', () => {
+        useDependencyStore().nodejs.version = '12.45.35'
         const nodecgStore = useNodecgStore()
         nodecgStore.status.installStatus = InstallStatus.READY_TO_INSTALL
         const wrapper = shallowMount(App, {
@@ -38,6 +52,7 @@ describe('App', () => {
     })
 
     it('hides bundle manager if nodecg is not installed and cannot be installed', () => {
+        useDependencyStore().nodejs.version = '12.44.2'
         const nodecgStore = useNodecgStore()
         nodecgStore.status.installStatus = InstallStatus.UNABLE_TO_INSTALL
         const wrapper = shallowMount(App, {
@@ -51,6 +66,7 @@ describe('App', () => {
     })
 
     it('hides bundle manager if nodecg status is unknown', () => {
+        useDependencyStore().nodejs.version = '13.45.2'
         const nodecgStore = useNodecgStore()
         nodecgStore.status.installStatus = InstallStatus.UNKNOWN
         const wrapper = shallowMount(App, {
--- src/store/__tests__/dependencyStore.test.ts
+++ src/store/__tests__/dependencyStore.test.ts
@@ -6,20 +6,20 @@ import Mock = jest.Mock
 jest.mock('@/service/dependencyService')
 
 describe('dependencyStore', () => {
-    const store = useDependencyStore()
-
     beforeEach(() => {
         setActivePinia(createPinia())
     })
 
     describe('hasNodejs', () => {
         it('is true if node.js version is set', () => {
+            const store = useDependencyStore()
             store.nodejs.version = '12.13.4'
 
             expect(store.hasNodejs).toEqual(true)
         })
 
         it('is false if node.js version is null', () => {
+            const store = useDependencyStore()
             store.$state.nodejs.version = null
 
             expect(store.hasNodejs).toEqual(false)
@@ -28,6 +28,7 @@ describe('dependencyStore', () => {
 
     describe('checkVersions', () => {
         it('gets node.js version', async () => {
+            const store = useDependencyStore();
             (getNodejsVersion as Mock).mockResolvedValue('156.53.3')
 
             await store.checkVersions()
