7c013113baca28c5a3adc28b5f669b3229a40247
inkfarer <inkfarer@inkfarer.com>
2024-04-02 09:35:37 +0300

    Rewrite NodeCG installation logic to support newer versions of NodeCG

--- src-tauri/src/npm.rs
+++ src-tauri/src/npm.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 use tauri::api::process::{Command, CommandEvent};
 use tauri::async_runtime::Receiver;
+
+#[derive(serde::Deserialize)]
+pub struct NPMPackageMetadata {
+    #[serde(rename = "dist-tags")]
+    pub dist_tags: HashMap<String, String>,
+    pub versions: HashMap<String, NPMPackageVersion>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct NPMPackageVersion {
+    pub dist: NPMPackageVersionDist,
+}
+
+#[derive(serde::Deserialize)]
+pub struct NPMPackageVersionDist {
+    pub tarball: String,
+}
 
 #[cfg(target_os = "windows")]
 pub fn install_npm_dependencies(path: &str) -> Result<Receiver<CommandEvent>, String> {
