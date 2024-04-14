2dd87feced7f064ed0fa029ce4057670297eb4b8
inkfarer <inkfarer@inkfarer.com>
2024-04-14 15:27:11 +0300

    Fix opening a path in the terminal on Windows briefly opening a second command prompt window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,12 +20,26 @@ use nodecg::ManagedNodecg;
 #[tauri::command(async)]
 fn open_path_in_terminal(path: String) -> Result<(), String> {
   if cfg!(target_os = "windows") {
-    return match std::process::Command::new("cmd")
-      .args(["/c", "start", "cmd.exe", "/k", &format!("cd /D {}", path)])
-      .spawn()
-    {
-      Ok(_) => Ok(()),
-      Err(e) => format_error("Failed to open path", e),
+    /*
+    Needs to be different when in development mode, or else the new command line will open in the
+    same command prompt that the application was originally launched from
+    */
+    return if cfg!(debug_assertions) {
+      match std::process::Command::new("cmd")
+          .args(["/c", "start", "cmd.exe", "/k", &format!("cd /D {}", path)])
+          .spawn()
+      {
+        Ok(_) => Ok(()),
+        Err(e) => format_error("Failed to open path", e),
+      }
+    } else {
+      match std::process::Command::new("cmd")
+          .args(["/k", &format!("cd /D {}", path)])
+          .spawn()
+      {
+        Ok(_) => Ok(()),
+        Err(e) => format_error("Failed to open path", e),
+      }
     };
   } else if cfg!(target_os = "macos") {
     return match std::process::Command::new("open")
